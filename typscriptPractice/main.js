0ac90a571f4d1751919864c6abaad30ef055fc11
jmf4nier <jmf4nier@gmail.com>
2019-09-05 19:01:07 -0500

    some tutorial typscript stuff added

--- typscriptPractice/main.js
+++ typscriptPractice/main.js
@@ -1,14 +1,27 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var messages = 'Hello world';
-console.log(messages);
+//console.log(messages);
 var x = 10; // allowed to instantiate 'let' variable, but 'const' must be initialized w/value
 var y = 20;
 var isBiginner = true;
 var total = 0;
 var name = 'Jason';
 var sentence = "my name is " + name + "     \nI am a beginner in typescript"; //can put a string on 2 lines and will print w/ on new line
-console.log(sentence);
+//console.log(sentence);
 var n = null;
 var u = undefined;
 var isNew = null;
@@ -27,19 +40,81 @@ var Color;
 })(Color || (Color = {}));
 ;
 var c = Color.Green;
-console.log(c); // will return 5
+//console.log(c)  // will return 5
 // assign datatype 'any' and 
 var randomValue = 10;
 randomValue = true;
 randomValue = 'something else';
 // very flexible and stops type errors.  can call methods on or call as function ect.
-var myVariable = 10;
+var myVariable = 'sarah';
 // typeAssertion  overides type checking
-function hasName(obj) {
-    return !!obj && typeof obj === "object" && "name" in obj;
-}
-if (hasName(myVariable)) {
-    console.log(myVariable.name);
-}
 var myVariable2 = 10;
 // (myVariable2 as string).toUpperCase();
+//union of types can be assigned either (maybe for user input)
+var multiType;
+multiType = 20;
+multiType = true;
+// function will require correct data type can use ? to denote optional argument (must always come after required args)
+//also can have default values.  if not given a new value the default will be assigned
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+;
+console.log(add(5));
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+// basically make a reusable 'type' using interface. cleaner code  
+function fullNameWithInterface(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Bruce',
+    lastName: 'wayne'
+};
+fullName(p);
+fullNameWithInterface(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var empl1 = new Employee('Jason');
+empl1.greet();
+// basic inheritance.  super + arg runs the employee constructor and make new employee with a name
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("manager delegateing task");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Henry');
+m1.greet();
+m1.delegateWork();
+// WONT work because it is 'protected' can only be used in class or child of base class---console.log(m1.employeeName)
+// m1.managerName is NOT a thing
+//public vs private.  public is default, private CANNOT be used outside of class
+var Dog = /** @class */ (function () {
+    function Dog(name) {
+        this.dogName = name;
+    }
+    Dog.prototype.callName = function () {
+        console.log(this.dogName);
+    };
+    return Dog;
+}());
+var d = new Dog('Fido');
+// this WONT work outside of class if private ---- console.log(d.dogName)
+d.callName(); //this WILL work because function is inside class
--- typscriptPractice/main.ts
+++ typscriptPractice/main.ts
@@ -1,7 +1,7 @@
 export {};
 
 let messages = 'Hello world';
-console.log(messages);
+//console.log(messages);
 
 let x = 10;                     // allowed to instantiate 'let' variable, but 'const' must be initialized w/value
 const y = 20;
@@ -12,7 +12,7 @@ let name: string = 'Jason';
 
 let sentence: string = `my name is ${name}     
 I am a beginner in typescript` ;             //can put a string on 2 lines and will print w/ on new line
-console.log(sentence);
+//console.log(sentence);
 
 let n: null = null;
 let u: undefined = undefined;
@@ -28,20 +28,101 @@ let person1: [string, number] = ['Jason', 35] ;  //only allowed 1 string and 1 n
 // enums value starts at 0 (indexes), can assign value also
 enum Color {Red, Green = 5, Blue};
 let c: Color = Color.Green;
-console.log(c)  // will return 5
+//console.log(c)  // will return 5
+
 // assign datatype 'any' and 
 let randomValue: any = 10;
 randomValue = true;
 randomValue = 'something else';
+
 // very flexible and stops type errors.  can call methods on or call as function ect.
-let myVariable: any = 10;
+let myVariable: any = 'sarah';
 
 // typeAssertion  overides type checking
-function hasName(obj: any): obj is { name: string}{
-    return !!obj && typeof obj === "object" && "name" in obj
-}
- if (hasName(myVariable)){
-     console.log(myVariable.name)
- }
+ 
+ 
 let myVariable2: unknown = 10;
 // (myVariable2 as string).toUpperCase();
+
+
+//union of types can be assigned either (maybe for user input)
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+// function will require correct data type can use ? to denote optional argument (must always come after required args)
+//also can have default values.  if not given a new value the default will be assigned
+function add(num1?: number, num2: number = 10): number {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1
+};
+console.log(add(5));
+
+function fullName(person: {firstName: string, lastName: string}){
+    console.log(`${person.firstName} ${person.lastName}`);
+}
+// basically make a reusable 'type' using interface. cleaner code  
+function fullNameWithInterface(person: Person){
+    console.log(`${person.firstName} ${person.lastName}`);
+
+}
+
+let p = {
+    firstName: 'Bruce',
+    lastName: 'wayne'
+}
+//interfaces can use '?' to note optional property.  useful for forms
+interface Person {
+    firstName: string,
+    lastName?: string;
+}
+fullName(p)
+fullNameWithInterface(p)
+
+class Employee {
+    protected employeeName: string;
+
+    constructor(name: string){
+        this.employeeName = name;
+    }
+
+    greet(){
+        console.log(`Good morning ${this.employeeName}`)
+    }
+}
+
+let empl1 = new Employee('Jason');
+empl1.greet()
+
+
+// basic inheritance.  super + arg runs the employee constructor and make new employee with a name
+class Manager extends Employee {
+    constructor(managerName: string){
+        super(managerName)
+    }
+    delegateWork(){
+        console.log(`manager delegateing task`)
+    }
+}
+let m1 = new Manager('Henry')
+m1.greet()
+m1.delegateWork()
+// WONT work because it is 'protected' can only be used in class or child of base class---console.log(m1.employeeName)
+// m1.managerName is NOT a thing
+
+//public vs private.  public is default, private CANNOT be used outside of class
+class Dog{
+    //public 
+    private dogName: string;
+    constructor(name: string){
+        this.dogName = name
+    }
+    callName(){
+        console.log(this.dogName)
+    }
+}
+let d = new Dog('Fido')
+// this WONT work outside of class if private ---- console.log(d.dogName)
+d.callName()  //this WILL work because function is inside class
